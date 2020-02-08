7f523cd8c9ce5487fb16b5db515ffdfbb2e0484f
Mohamed-Albakosh <azzrow@gmail.com>
2020-02-08 15:41:13 -0800

    Adding basic javascript assignment
    basic javascript

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,16 @@
+//window.alert();
+var A="this is a string";
+var family="Albakosh", dad="Sleman", son="Axel";
+document.write("Hello, World!"+A);
+document.write("lisa told,\he n'\ man");
+var name = "Mohamed"+"Albakosh";
+document.write(name,dad);
+var blues = "I have the blies";
+var blues = blues.fontcolor("blue");
+document.write(blues);
+document.write(3+3);
+function MY_First_Function(){
+    var str = "This text is green!";
+    var result = str.fontcolor("green");
+    document.write("Green_Text").innerHTML= result;
+}
