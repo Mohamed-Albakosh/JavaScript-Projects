1c8f3af3b2c3bd5c659f7b6133904a30ead729a9
Mohamed-Albakosh <azzrow@gmail.com>
2020-02-10 19:01:16 -0800

    adding my javascript update
    my assignment

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_2.js
@@ -0,0 +1,10 @@
+function my_assingment_function(){
+    var nam_1 = "Mohamed";
+    var result = nam_1.fontcolor("green");
+     document.getElementById("di").innerHTML = result;
+}_
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += "a lot from this book";
+    document.getElementById("Concatenate").innerHTML = sentence ;
+}
